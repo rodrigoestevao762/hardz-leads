6161f95ec4befa82a4aa2360d8b2b706fb71dad5
Rodrigo Estevao <rodrigoestevao762@gmail.com>
2026-09-16 12:57:45 -0300

    fix: typings on api route cookies

--- app/api/enrich/route.ts
+++ app/api/enrich/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: Request) {
           getAll() {
             return cookieStore.getAll()
           },
-          setAll(cookiesToSet) {
+          setAll(cookiesToSet: any[]) {
             try {
               cookiesToSet.forEach(({ name, value, options }) =>
                 cookieStore.set(name, value, options)
